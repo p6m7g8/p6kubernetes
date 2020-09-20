d25d631b559edc6ca76f9352f9fabdbe511976f3
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-09-19 20:02:43 -0400

    feat(api): move eksctl code out

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ### init.zsh:
+- p6_kubernetes_prompt_info()
+- p6_kubernetes_status()
 - p6df::modules::p6kubernetes::deps()
 - p6df::modules::p6kubernetes::init()
 - p6df::modules::p6kubernetes::version()
--- init.zsh
+++ init.zsh
@@ -25,101 +25,51 @@ p6df::modules::p6kubernetes::deps() { ModuleDeps=() }
 ######################################################################
 p6df::modules::p6kubernetes::init() { }
 
-# p6kubernetes
 ######################################################################
 #<
 #
-# Function: p6_kubernetes_cluster_create(cluster_name, [cluster_version=1.17], [vpc_id=$AWS_VPC_ID])
-#
-#  Args:
-#	cluster_name - 
-#	OPTIONAL cluster_version -  [1.17]
-#	OPTIONAL vpc_id -  [$AWS_VPC_ID]
+# Function: p6_kubernetes_status()
 #
 #>
 ######################################################################
-p6_kubernetes_cluster_create() {
-  local cluster_name="$1"
-  local cluster_version="${2:-1.17}"
-  local vpc_id="${3:-$AWS_VPC_ID}"
+p6_kubernetes_status() {
 
-  local private_subnets
-  local public_sunets
+  local resource_types="configmaps deployment ingress pods pvc svc secret statefulset"
 
-  eksctl create cluster \
-    --name "$cluster_name" \
-    --version "$cluster_version" \
-    --without-nodegroup \
-    --vpc-private-subnets "$private_subnets" \
-    --vpc-public-subnets "$public_subnets"
+  local type
+  for type in $(p6_echo $resource_types); do
+    p6_h1 "$type"
+    kubectl get $type -A
+  done
 }
 
-# p6aws/lib/svc/eks
 ######################################################################
 #<
 #
-# Function: p6_kubernetes_aws_cluster_logging_enable([cluster_name=$KUBE_CLUSTER_NAME])
+# Function: p6_kubernetes_prompt_info()
 #
-#  Args:
-#	OPTIONAL cluster_name -  [$KUBE_CLUSTER_NAME]
-#
-#>
-######################################################################
-p6_kubernetes_aws_cluster_logging_enable() {
-  local cluster_name="${1:-$KUBE_CLUSTER_NAME}"
-
-  aws eks update-cluster-config \
-    --name "$cluster_name" \
-    --logging '{"clusterLogging":[{"types":["api","audit","authenticator","controllerManager","scheduler"],"enabled":true}]}'
-}
-
-# ALB Ingress Controller Only
-######################################################################
-#<
-#
-# Function: p6_kubernetes_aws_cluster_fargate_profile_create([cluster_name=$KUBE_CLUSTER_NAME], profile_name, namespace)
-#
-#  Args:
-#	OPTIONAL cluster_name -  [$KUBE_CLUSTER_NAME]
-#	profile_name - 
-#	namespace - 
+#  Returns:
+#	str - str
 #
 #>
 ######################################################################
-p6_kubernetes_aws_cluster_fargate_profile_create() {
-  local cluster_name="${1:-$KUBE_CLUSTER_NAME}"
-  local profile_name="$2"
-  local namespace="$3"
+p6_kubernetes_prompt_info() {
 
-  # XXX: use config file with templates for multiple namespaces
-  eksctl create fargateprofile \
-    --cluster "$cluster_name" \
-    --name "$profile_name" \
-    --namespace "$namespace"
-}
+    local str
 
-# NLB
-######################################################################
-#<
-#
-# Function: p6_kunernetes_nodegroup_create([cluster_name=$KUBE_CLUSTER_NAME], profile_name)
-#
-#  Args:
-#	OPTIONAL cluster_name -  [$KUBE_CLUSTER_NAME]
-#	profile_name - 
-#
-#>
-######################################################################
-p6_kunernetes_nodegroup_create() {
-  local cluster_name="${1:-$KUBE_CLUSTER_NAME}"
-  local profile_name="$2"
+    if ! p6_string_blank "$KUBECONFIG"; then
+      str="kube:   "
+    fi
+    if ! p6_string_blank "$P6_KUBE_CFG"; then
+      str="${str}ctx=[$P6_KUBE_CFG]"
+    fi
+    if ! p6_string_blank "$P6_KUBE_NS"; then
+      str="${str} ns=[$P6_KUBE_NS]"
+    fi
 
-  eksctl create nodegroup \
-    --cluster $cluster_name \
-    --name $profile_name \
-    --managed \
-    --node-type m5.large \
-    --nodes 3 \
-    --nodes-min 2 \
-    --nodes-max 4
+    if p6_string_blank "$str"; then
+      p6_return_void
+    else
+      p6_return_str "$str"
+    fi
 }
